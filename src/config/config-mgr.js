5a84e7477efab67b842f84f12ce16ff1923ca498
Ritik Patidar <67598349+Ritik-Patidar@users.noreply.github.com>
2024-08-05 12:46:43 +0530

    replace pkg-up with cosmiconfig to fetch all the configuration from where the command is executed

--- src/config/config-mgr.js
+++ src/config/config-mgr.js
@@ -1,14 +1,15 @@
 import chalk from 'chalk';
-import {pkgUp} from 'pkg-up';
+import { cosmiconfigSync } from 'cosmiconfig';
+
+const configLoader = cosmiconfigSync('tool');
 
 export function getConfig() {
-    const pkgPath = pkgUp.sync({ cwd: process.cwd() });
-    const pkg = require(pkgPath);
-    if (pkg.tool) {
-        console.log('Found configuration', pkg.tool);
-        return pkg.tool;
-    } else {
+    const result = configLoader.search(process.cwd());
+    if (!result) {
         console.log(chalk.yellow('Could not find configuration, using default'));
         return { port: 1234 };
+    } else {
+        console.log('Found configuration', result.config);
+        return result.config;
     }
 }
